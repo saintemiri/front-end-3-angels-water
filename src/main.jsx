136d6a044c50ae420d7f0ea8633cb200b3d10e49
Gian Carlo Angeles <larri81737881@gmail.com>
2026-09-05 14:24:05 +0800

    Resolve merge conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
 import './styles/index.css'
 
@@ -11,16 +11,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-=======
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
->>>>>>> b2f005ff15be988f2cc6db0af04a19a2c70a995e
 )
